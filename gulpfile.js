d833fb07353c706ac7ec47709cc8ba0446a7753d
Floren de Gier <florendg@gmail.com>
2016-08-22 22:08:03 +0200

    Eerste opzet SASS

--- gulpfile.js
+++ gulpfile.js
@@ -5,24 +5,40 @@
  */
 var gulp = require('gulp');
 var del = require('del');
+var sass = require('gulp-sass');
+var notify = require('gulp-notify');
 
 var paths = {
+    nodeDir: "./node_modules",
     assets: {
-        bootstrap: "node_modules/bootstrap/dist"
+        bootstrap: "node_modules/bootstrap/dist",
+        bootstrapsass: "node_modules/bootstrap-sass/assets"
     },
     src: {
         style: "src/resources/style"
-    }
+    },
 };
 
 /** Default task.
  * This one is called when gulp is exucuted without arguments.
  */
-gulp.task('default', function () {
+gulp.task('default',['sass'], function () {
     gulp.src(["index.html",
         paths.assets.bootstrap + "/**/*.css",
         paths.assets.bootstrap + "/**/*.css.map"])
         .pipe(gulp.dest("dist"));
+});
+
+gulp.task('sass',function(){
+    gulp.src('src/app/style/style.scss')
+        .pipe(sass(sass({
+            style: 'compressed',
+            loadPath: [
+                paths.nodeDir + '/bootstrap-sass/assets/stylesheets'
+            ]
+        }) ).on("error", notify.onError(function (error) {
+        return "Error: " + error.message;})))
+        .pipe(gulp.dest('dist/css'));
 });
 
 /**
